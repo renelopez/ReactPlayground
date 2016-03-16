fe32bb7c4a3d32f6d19e43ae9ac6199b1f428403
rene.lopez <rene.lopez.cano@gmail.com>
2016-03-16 12:42:11 -0600

    Switched to registry

--- deploy.cmd
+++ deploy.cmd
@@ -75,7 +75,7 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
 
   SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
 ) ELSE (
-  SET NPM_CMD=npm
+  SET NPM_CMD=npm --registry "http://registry.npmjs.org/"
   SET NODE_EXE=node
 )
 
