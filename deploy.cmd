117e759bac57b1b7bb01a3fa8bff19e408d990a9
rene.lopez <rene.lopez.cano@gmail.com>
2016-03-16 13:21:08 -0600

    Updated NPM

--- deploy.cmd
+++ deploy.cmd
@@ -73,7 +73,8 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
     SET NODE_EXE=node
   )
 
-  SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
+  ::SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
+  SET NPM_CMD=npm --registry "http://registry.npmjs.org/"
 ) ELSE (
   SET NPM_CMD=npm --registry "http://registry.npmjs.org/"
   SET NODE_EXE=node
