01dd7fe1e61165166cbbdb007d8764d48f75a45f
rene.lopez <rene.lopez.cano@gmail.com>
2016-03-16 12:54:30 -0600

    Added logs

--- deploy.cmd
+++ deploy.cmd
@@ -60,12 +60,12 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
   IF !ERRORLEVEL! NEQ 0 goto error
 
   IF EXIST "%DEPLOYMENT_TEMP%\__nodeVersion.tmp" (
-    SET /p NODE_EXE="%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
+    SET /p NODE_EXE<="%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
   )
   
   IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
-    SET /p NPM_JS_PATH="%DEPLOYMENT_TEMP%\__npmVersion.tmp"
+    SET /p NPM_JS_PATH<="%DEPLOYMENT_TEMP%\__npmVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
   )
 
